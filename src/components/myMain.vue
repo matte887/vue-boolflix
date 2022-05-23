b0464492212cb174efae8e766d13e167220c088f
matte887 <m.erroi92@gmail.com>
2022-05-23 16:50:26 +0200

    Aggiunta copertina

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -3,25 +3,26 @@
     <h2>Movies</h2>
     <ul>
       <li class="movie-card" v-for="(item, index) in moviesList" :key="index">
+        <img :src="imgLinkRoot + item.poster_path" alt="" />
         <p>Title: {{ item.original_title }}</p>
         <div class="movie-language">
           Original language: {{ item.original_language }}
           <img :src="pickFlag(item)" alt="" />
         </div>
         <p>Original title: {{ item.title }}</p>
-        <p>Vote: {{ item.vote_average }}/10</p>
+        <p>Vote: {{ convertVote(item.vote_average) }}</p>
       </li>
     </ul>
     <h2>TV Shows</h2>
     <ul>
       <li class="movie-card" v-for="(item, index) in TVShowsList" :key="index">
-        <p>Title: {{item.name}}</p>
+        <p>Title: {{ item.name }}</p>
         <div class="movie-language">
           Original language: {{ item.original_language }}
           <img :src="pickFlag(item)" alt="" />
         </div>
         <p>Original title: {{ item.name }}</p>
-        <p>Vote: {{ item.vote_average }}/10</p>
+        <p>Vote: {{ convertVote(item.vote_average) }}</p>
       </li>
     </ul>
   </main>
@@ -34,12 +35,13 @@ export default {
     return {
       italianFlag: "assets/img/IT.webp",
       usaFlag: "assets/img/USA.png",
-      globeFlag: "assets/img/globe.png"
+      globeFlag: "assets/img/globe.png",
+      imgLinkRoot: "http://image.tmdb.org/t/p/w500/",
     };
   },
   props: {
     moviesList: Array,
-    TVShowsList: Array
+    TVShowsList: Array,
   },
   methods: {
     pickFlag(item) {
@@ -51,6 +53,9 @@ export default {
         return this.globeFlag;
       }
     },
+    convertVote(vote) {
+      console.log(vote);
+    }
   },
 };
 </script>
@@ -67,6 +72,10 @@ ul {
       width: 25px;
       height: auto;
       margin-left: 0.2rem;
+    }
+    img {
+      height: 200px;
+      width: auto;
     }
   }
 }
