dfdb8a93af7aaa5b9acde3bb52a5172fa3a3da8f
matte887 <m.erroi92@gmail.com>
2022-05-23 14:44:13 +0200

    Milestone 1: app stampa in pagina tutti film corrispondenti

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <myHeader @searchEvent='filterMovies($event)' />
+  <div id="app">
+    <myHeader @searchEvent='filterMovies($event)' />
+    <myMain :moviesList='searchedMovies' />
+  </div>
 </template>
 
 <script>
-import myHeader from './components/myHeader.vue'
+import myHeader from './components/myHeader.vue';
+import myMain from './components/myMain.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    myHeader
+    myHeader,
+    myMain
+  },
+  data() {
+    return {
+      searchedMovies: [],
+      researchLink: 'https://api.themoviedb.org/3/search/movie?api_key=e6270ee8d8674f73682b91ddb2f17fe5&query=',
+      completeResearchLink: ''
+    }
   },
   methods: {
     filterMovies(searchKey) {
-      console.log(searchKey);
+      this.createResearchLink(searchKey);
+      axios
+        .get(this.completeResearchLink)
+        .then((resp) => {
+          this.searchedMovies = resp.data.results;
+        });
+    },
+    createResearchLink(searchKey) {
+      this.completeResearchLink = this.researchLink + searchKey;
     }
   }
 }
---
+++ src/components/myMain.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+      <ul>
+          <li class="movie-card" v-for="(item, index) in moviesList" :key="index">
+              <p>Title: {{item.original_title}}</p>
+              <p>Original language: {{item.original_language}}</p>
+              <p>Original title: {{item.title}}</p>
+              <p>Vote: {{item.vote_average}}/10</p>
+          </li>
+      </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "myMain",
+  props: {
+    moviesList: Array
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+    list-style: none;
+    .movie-card {
+        border: 1px solid black;
+        display: inline-block;
+        margin: 1rem;
+        padding: 1rem;
+    }
+}
+</style>
