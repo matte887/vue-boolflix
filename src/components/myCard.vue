1ac2da56df0f555c90e00dec7df5398d354dada1
matte887 <m.erroi92@gmail.com>
2022-05-23 18:25:16 +0200

    Improved vue file structure

---
+++ src/components/myCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <li class="movie-card">
+      <img :src="imgLinkRoot + showData.poster_path" alt="" />
+      <p>Title: {{ showData.original_title }}</p>
+      <div class="movie-language">
+        Original language: {{ showData.original_language }}
+        <img :src="pickFlag(showData)" alt="" />
+      </div>
+      <p>Original title: {{ showData.title }}</p>
+      <p>Vote: {{ convertVote(showData.vote_average) }} <i class="fa-solid fa-star"></i></p>
+    </li>
+</template>
+
+<script>
+export default {
+  name: "myCard",
+  props: {
+    showData: Object,
+  },
+  data() {
+    return {
+      italianFlag: "assets/img/IT.webp",
+      usaFlag: "assets/img/USA.png",
+      globeFlag: "assets/img/globe.png",
+      imgLinkRoot: "http://image.tmdb.org/t/p/w500/",
+      stars: "",
+    };
+  },
+  methods: {
+    pickFlag(item) {
+      if (item.original_language === "it") {
+        return this.italianFlag;
+      } else if (item.original_language === "en") {
+        return this.usaFlag;
+      } else {
+        return this.globeFlag;
+      }
+    },
+    convertVote(vote) {
+      const convertedVote = vote / 2;
+      const roundedVote = Math.ceil(convertedVote);
+      return roundedVote;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.movie-card {
+  border: 1px solid black;
+  display: inline-block;
+  margin: 1rem;
+  padding: 1rem;
+  .movie-language img {
+    width: 25px;
+    height: auto;
+    margin-left: 0.2rem;
+  }
+  img {
+    height: 200px;
+    width: auto;
+  }
+}
+</style>
--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,82 +1,30 @@
 <template>
   <main>
-    <!-- <i class="far fa-star"></i>
-    <i class="fas fa-star"></i> -->
     <h2>Movies</h2>
     <ul>
-      <li class="movie-card" v-for="(item, index) in moviesList" :key="index">
-        <img :src="imgLinkRoot + item.poster_path" alt="" />
-        <p>Title: {{ item.original_title }}</p>
-        <div class="movie-language">
-          Original language: {{ item.original_language }}
-          <img :src="pickFlag(item)" alt="" />
-        </div>
-        <p>Original title: {{ item.title }}</p>
-        <p>Vote: {{ convertVote(item.vote_average) }}</p>
+      <li>
+        <myCard v-for="(item, index) in moviesList" :key="index" :showData="item" />
       </li>
     </ul>
     <h2>TV Shows</h2>
     <ul>
-      <li class="movie-card" v-for="(item, index) in TVShowsList" :key="index">
-        <p>Title: {{ item.name }}</p>
-        <div class="movie-language">
-          Original language: {{ item.original_language }}
-          <img :src="pickFlag(item)" alt="" />
-        </div>
-        <p>Original title: {{ item.name }}</p>
-        <p>Vote: {{ convertVote(item.vote_average) }} <i class="far fa-star"></i></p>
+      <li>
+      <myCard v-for="(item, index) in TVShowsList" :key="index" :showData="item" />
       </li>
     </ul>
   </main>
 </template>
 
 <script>
+import myCard from "./myCard.vue"
 export default {
   name: "myMain",
-  data() {
-    return {
-      italianFlag: "assets/img/IT.webp",
-      usaFlag: "assets/img/USA.png",
-      globeFlag: "assets/img/globe.png",
-      imgLinkRoot: "http://image.tmdb.org/t/p/w500/",
-      stars: '',
-    };
+  components: {
+    myCard
   },
   props: {
     moviesList: Array,
     TVShowsList: Array,
-  },
-  methods: {
-    pickFlag(item) {
-      if (item.original_language === "it") {
-        return this.italianFlag;
-      } else if (item.original_language === "en") {
-        return this.usaFlag;
-      } else {
-        return this.globeFlag;
-      }
-    },
-    convertVote(vote) {
-      const convertedVote = vote / 2;
-      const roundedVote = Math.ceil(convertedVote);
-      this.voteToStars(roundedVote);
-      console.log(this.voteToStars);
-      return this.stars;
-    },
-    voteToStars(numberOfStars) {
-      let starString = '';
-      for (let i = 0; i < numberOfStars; i++) {
-        starString += '*';
-      }
-      const whiteStars = 5 - numberOfStars;
-      if (whiteStars > 0) {
-        for (let i = 0; i < whiteStars; i++) {
-          starString += '-';
-        }
-      }
-      this.stars = starString;
-      return starString;
-    }
   },
 };
 </script>
@@ -84,20 +32,6 @@ export default {
 <style lang="scss" scoped>
 ul {
   list-style: none;
-  .movie-card {
-    border: 1px solid black;
-    display: inline-block;
-    margin: 1rem;
-    padding: 1rem;
-    .movie-language img {
-      width: 25px;
-      height: auto;
-      margin-left: 0.2rem;
-    }
-    img {
-      height: 200px;
-      width: auto;
-    }
-  }
+  // display: inline-block;
 }
 </style>
