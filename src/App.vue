a1331d49367a38d53f1e6363851c200c74f93ecb
matte887 <m.erroi92@gmail.com>
2022-05-23 12:44:07 +0200

    Creato header e passata chiave ricerca

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <myHeader @searchEvent='filterMovies($event)' />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myHeader from './components/myHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myHeader
+  },
+  methods: {
+    filterMovies(searchKey) {
+      console.log(searchKey);
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/myHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+      <form>
+          <input type="text" placeholder="Cerca un film..." v-model="searchKey">
+          <button type="button" @click="$emit('searchEvent', searchKey)">Search</button>
+      </form>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'myHeader',
+    data() {
+        return {
+            searchKey: ''
+        }
+    },
+    methods: {
+        searchMovie() {
+            return console.log('ciao');
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
