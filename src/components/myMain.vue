68f613956fe0a92b71b10bc90bcc5527286d3504
matte887 <m.erroi92@gmail.com>
2022-05-23 15:50:40 +0200

    Implementando bandiera

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,33 +1,53 @@
 <template>
   <main>
-      <ul>
-          <li class="movie-card" v-for="(item, index) in moviesList" :key="index">
-              <p>Title: {{item.original_title}}</p>
-              <p>Original language: {{item.original_language}}</p>
-              <p>Original title: {{item.title}}</p>
-              <p>Vote: {{item.vote_average}}/10</p>
-          </li>
-      </ul>
+    <!-- <img src="../assets/img/IT.webp" alt=""> -->
+    <ul>
+      <li class="movie-card" v-for="(item, index) in moviesList" :key="index">
+        <p>Title: {{ item.original_title }}</p>
+        <div class="movie-language">
+          Original language: {{ item.original_language }}
+          <img :src="pickFlag(item)" alt="" />
+        </div>
+        <p>Original title: {{ item.title }}</p>
+        <p>Vote: {{ item.vote_average }}/10</p>
+      </li>
+    </ul>
   </main>
 </template>
 
 <script>
 export default {
   name: "myMain",
+  data() {
+    return {
+      italianFlag: "../assets/img/IT.webp",
+    };
+  },
   props: {
-    moviesList: Array
+    moviesList: Array,
+  },
+  methods: {
+    pickFlag(item) {
+      if (item.original_language === "it") {
+        return this.italianFlag;
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 ul {
-    list-style: none;
-    .movie-card {
-        border: 1px solid black;
-        display: inline-block;
-        margin: 1rem;
-        padding: 1rem;
+  list-style: none;
+  .movie-card {
+    border: 1px solid black;
+    display: inline-block;
+    margin: 1rem;
+    padding: 1rem;
+    .movie-language img {
+      width: 20px;
+      margin-left: 0.2rem;
     }
+  }
 }
 </style>
