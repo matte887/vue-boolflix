29fb0eed10e347366f13b2711759febbcd67d1b5
matte887 <m.erroi92@gmail.com>
2022-05-26 11:15:36 +0200

    Gestita assenza cover image

--- src/components/myCard.vue
+++ src/components/myCard.vue
@@ -1,20 +1,29 @@
 <template>
   <li class="col" @mouseleave="handleLeave" @mouseover="handleHover">
     <div class="movie-card">
-      <!-- Show cover -->
-      <img
-        :src="imgLinkRoot + showData.poster_path"
-        alt=""
-        v-if="hover == false"
-      />
-      <!-- /Show cover -->
 
-      <!-- Show info -->
-      <div class="hover-info px-3 pt-4" v-if="hover">
+      <!-- CARD FRONT: show cover -->
+      <div class="card-front" v-if="hover == false">
+        <img
+          :src="imgLinkRoot + showData.poster_path"
+          alt=""
+          v-if="showData.poster_path"
+        />
+        <div class="missing-cover" v-else>
+          <p>Missing cover image</p>
+          <span class="smile-sad">: (</span>
+        </div>
+      </div>
+      <!-- /CARD FRONT: show cover -->
+
+      <!-- CARD BACK: show info -->
+      <div class="card-back hover-info px-3 pt-4" v-if="hover">
         <!-- Titolo dello show -->
         <div class="show-title">
           <h5>
-            {{ showData.original_title ? showData.original_title : showData.name}}
+            {{
+              showData.original_title ? showData.original_title : showData.name
+            }}
           </h5>
         </div>
         <!-- /Titolo dello show -->
@@ -25,32 +34,49 @@
           {{ showData.original_language }}
           <!-- Mio metodo per bandiere -->
           <!-- <img :src="pickFlag(showData)" alt="" /> -->
-          <img v-if="languageHasImage()" :src="require(`../assets/img/${showData.original_language}.png`)" alt="">
-          <img v-else class="globe" :src="require('../assets/img/globe.png')" alt="">
+          <img
+            v-if="languageHasImage()"
+            :src="require(`../assets/img/${showData.original_language}.png`)"
+            alt=""
+          />
+          <img
+            v-else
+            class="globe"
+            :src="require('../assets/img/globe.png')"
+            alt=""
+          />
         </div>
         <!-- /Lingua originale -->
 
         <!-- Titolo originale (lo mostro solo se diverso dal titolo) -->
-        <div v-if="showData.title !== showData.original_title || showData.original_name !== showData.name">
+        <div
+          v-if="
+            showData.title !== showData.original_title ||
+            showData.original_name !== showData.name
+          "
+        >
           <span class="fw-bold">Original title:</span>
           <span>
-            {{ showData.title ? showData.title : showData.original_name}}
+            {{ showData.title ? showData.title : showData.original_name }}
           </span>
-
         </div>
         <!-- /Titolo originale -->
 
         <!-- cast -->
         <span class="fw-bold">Cast: </span>
         <ul>
-          <li v-for="(item, index) in showData.cast" :key="index">{{item.name}}</li>
+          <li v-for="(item, index) in showData.cast" :key="index">
+            {{ item.name }}
+          </li>
         </ul>
         <!-- /cast -->
 
         <!-- genere -->
         <p>
           <span class="fw-bold">Genere: </span>
-          <span v-for="(item, index) in showData.genres" :key="index">{{item.name}}, </span>
+          <span v-for="(item, index) in showData.genres" :key="index"
+            >{{ item.name }},
+          </span>
         </p>
         <!-- /genere -->
 
@@ -72,7 +98,8 @@
         </span>
         <!-- /Stringa stelle -->
       </div>
-      <!-- /Show info -->
+      <!-- /CARD BACK: show info -->
+
     </div>
   </li>
 </template>
@@ -91,7 +118,7 @@ export default {
       // globeFlag: "assets/img/globe.png",
       imgLinkRoot: "http://image.tmdb.org/t/p/w342/",
       hover: false,
-      flags:["en", "it"]
+      flags: ["en", "it"],
     };
   },
   methods: {
@@ -132,29 +159,45 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/variables.scss";
-.col {
-  .movie-card {
+.movie-card {
+  height: 100%;
+  .card-front {
     height: 100%;
+  }
+  img {
+    height: auto;
+    width: 100%;
+  }
+
+  .missing-cover {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    border: 1px solid white;
+    .smile-sad {
+      writing-mode: tb;
+      font-size: 1.5rem;
+    }
+  }
+
+  .hover-info {
+    background-color: black;
+    height: 100%;
+    .stars {
+      color: $star-rating;
+    }
+  }
+  .movie-language {
     img {
+      width: 20px;
       height: auto;
-      width: 100%;
+      margin-left: 0.2rem;
     }
-    .hover-info {
-      background-color: black;
-      height: 100%;
-      .stars {
-        color: $star-rating;
-      }
-    }
-    .movie-language {
-      img {
-        width: 20px;
-        height: auto;
-        margin-left: 0.2rem;
-      }
-      .globe {
-        filter: invert(1);
-      }
+    .globe {
+      filter: invert(1);
     }
   }
 }
